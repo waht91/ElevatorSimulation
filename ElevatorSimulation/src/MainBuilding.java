80566fd52825438009e24cb8152f523be27a13ee
waht9 <waht9@DESKTOP-J6TR41A.fios-router.home>
2018-05-15 23:48:30 -0400

    2

--- ElevatorSimulation/src/MainBuilding.java
+++ ElevatorSimulation/src/MainBuilding.java
@@ -67,7 +67,7 @@ public class MainBuilding {
 			if(controller.checkFloors()){
 				if(controller.checkAvailability ()){
 					controller.requestE();
-					//Request elevator and assign one
+					//Request elevator and assign one Succesfully Puhsed
 				}
 			}
 			controller.timepassone ();
